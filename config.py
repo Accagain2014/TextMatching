363c3e5d80307115460d78c2415d3355d106c5a9
cms <chenmaosen0@gmail.com>
2017-04-25 16:06:56 +0800

    change wordhash to support sparse matrix output

--- config.py
+++
@@ -1,4 +0,0 @@
-
-train_file = '/Users/cms/UCAS/material/paper/TextMatching/dataset/quora/train_porter_rm_stopwords.csv'
-
-test_file = '/Users/cms/UCAS/material/paper/TextMatching/dataset/quora/test_porter_rm_stopwords.csv'
